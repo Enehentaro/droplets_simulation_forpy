f8f38d4dee77c6f4c84ce1c4057ab0d6822deb9d
KishiShohei <sk2.090412066@gmail.com>
2022-08-21 13:49:21 +0900

    functionalize nearest_search

--- droplets_simulation/lib/unstructured_grid.py
+++ droplets_simulation/lib/unstructured_grid.py
@@ -3,12 +3,6 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.neighbors import NearestNeighbors 
 
-np.random.seed(19)
-
-data = np.random.random_sample((10,3))
-point = [0.5,0.5,0.5]
-print(data)
-
 # fig = plt.figure()
 # ax = fig.add_subplot(111, projection='3d')
 
@@ -19,6 +13,15 @@ print(data)
 # ax.scatter([i[0] for i in data], [i[1] for i in data], [i[2] for i in data], cmap='jet',label="z")
 # ax.scatter(point[0],point[1],point[2])
 
-knn_model = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(data) 
-distance, nearestID = knn_model.kneighbors([point])
-print(nearestID[0])
+def nearest_search(data:np.ndarray, point:np.ndarray) -> list:
+    knn_model = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(data) 
+    distance, nearestID = knn_model.kneighbors([point])
+    return nearestID[0]
+
+if __name__ == '__main__':
+    np.random.seed(19)
+    data = np.random.random_sample((10,3))
+    point = np.array([0.5,0.5,0.5])
+    nearestID = nearest_search(data, point)
+
+    print(nearestID)
