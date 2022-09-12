7173642617dab87fc41d4e42bde0a92be193a349
KishiShohei <sk2.090412066@gmail.com>
2022-09-12 14:47:06 +0900

    add method into cli.py

--- droplets_simulation/cli.py
+++ droplets_simulation/cli.py
@@ -1,11 +1,22 @@
-from lib.setting_reader import read_setting
+from lib.setting_reader import *
 from lib.SimpleVtk import SimpleVtkUnstructuredGrid
+from lib.droplet import *
 
 def main():
+    ugrid = SimpleVtkUnstructuredGrid()
+
     print("please write your case directory name")
     CaseDir = input()
+
     droplet_setting, flow_setting = read_setting(CaseDir)
-    ugrid = SimpleVtkUnstructuredGrid()
+    dropSetPlace = read_dropSetPlace(CaseDir)
+
+    dropIniPlace = calc_dropIniPlace(droplet_setting["num_droplets"],dropSetPlace)
+    dropIniRadius = read_dropIniRadius(droplet_setting["num_droplets"])
+    dGroup = get_dropletArray(droplet_setting["num_droplets"], dropIniPlace, dropIniRadius)
+    
+    output_dropVTK(droplet_setting["num_droplets"],dGroup)
+    
     ugrid.read_vtk(flow_setting["path2FlowFile"])
 
 if __name__ == '__main__':
--- droplets_simulation/lib/droplet.py
+++ droplets_simulation/lib/droplet.py
@@ -1,6 +1,4 @@
 import numpy as np
-from setting_reader import read_setting, read_dropSetPlace
-from SimpleVtk import SimpleVtkUnstructuredGrid
 import random
 
 droplet_dtype = np.dtype([
@@ -42,7 +40,7 @@ def calc_dropIniPlace(num_droplets:int,dropSetPlace:np.ndarray) -> np.ndarray:
 
     
 def read_dropIniRadius(num_droplets:int) -> np.ndarray:
-    IniRad_ratio = np.loadtxt('../../data/initialRadius_coughing.txt', delimiter=",", skiprows=1, dtype='float')
+    IniRad_ratio = np.loadtxt('../data/initialRadius_coughing.txt', delimiter=",", skiprows=1, dtype='float')
     IniRad = IniRad_ratio[:,0]*1.e-6
     ratio = IniRad_ratio[:,1]
     random.seed(0)
@@ -54,19 +52,24 @@ def read_dropIniRadius(num_droplets:int) -> np.ndarray:
 
     return np.array(dropIniRad)
 
+def output_dropVTK(num_droplets:int,dGroup:np.ndarray):
+    from lib.SimpleVtk import SimpleVtkUnstructuredGrid
+    output = SimpleVtkUnstructuredGrid()
+    cell_types = np.ones(num_droplets, dtype = np.int64)
+    offsets = np.arange(num_droplets, dtype = np.int64)
+    cell2node = np.arange(num_droplets, dtype = np.int64)
+    output.set_points(dGroup["position"])
+    output.set_cells(offsets, cell2node, cell_types)
+    output.add_field_cell_data("radius", dGroup["radius"], "scalar")
+    output.make_grid()
+    output.write_out("../vtk/drop.vtk")
+
 if __name__ == '__main__':
+    from setting_reader import read_setting, read_dropSetPlace
     droplet_setting, flow_setting = read_setting("../../case")
     dropSetPlace = read_dropSetPlace("../../case")
     dropIniRadius = read_dropIniRadius(droplet_setting["num_droplets"])
     dropIniPlace = calc_dropIniPlace(droplet_setting["num_droplets"],dropSetPlace)
     dGroup = get_dropletArray(droplet_setting["num_droplets"], dropIniPlace, dropIniRadius)
 
-    output = SimpleVtkUnstructuredGrid()
-    cell_types = np.ones(droplet_setting["num_droplets"], dtype = np.int64)
-    offsets = np.arange(droplet_setting["num_droplets"], dtype = np.int64)
-    cell2node = np.arange(droplet_setting["num_droplets"], dtype = np.int64)
-    output.set_points(dGroup["position"])
-    output.set_cells(offsets, cell2node, cell_types)
-    output.add_field_cell_data("radius", dGroup["radius"], "scalar")
-    output.make_grid()
-    output.write_out("../../vtk/drop.vtk")
+    output_dropVTK(droplet_setting["num_droplets"],dGroup)
